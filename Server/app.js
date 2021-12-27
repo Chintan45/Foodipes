d894bc9602a689a7df16f2a4f1f6d336cdea3bc0
Chintan Dobariya <65342816+ChintanDobariya@users.noreply.github.com>
2021-12-27 13:54:10 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -35,35 +35,6 @@ app.get('/posts', (req, res) => {
     })
 })
 
-app.get('/recipes_data/:id', (req, res) => {
-    const recipe = recipes_data.find(c => c.id === parseInt(req.params.id))
-    if (!recipe) res.status(404).send('Given id not found..')
-    res.send(recipe);
-})
-
-app.post('/recipes_data', (req, res) => {
-
-    fs.readFile(__dirname + '/' + 'recipes_data.json', 'utf-8', (err, data) => {
-        let recipes_data = JSON.parse(data);
-        const recipe = {
-            id: recipes_data.length + 1,
-            title: req.body.title,
-            url: req.body.url,
-            rating: req.body.rating,
-            author: req.body.author,
-            keywords: req.body.keywords
-        };
-
-        recipes_data = [...recipes_data, recipe]
-
-        fs.writeFileSync(__dirname + '/' + 'recipes_data.json', JSON.stringify(recipes_data, null, 2), (err) => {
-            if (err) consol.log(err);
-        })
-        let x = recipes_data;
-        res.send(x);
-    })
-
-})
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
